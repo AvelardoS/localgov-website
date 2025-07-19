d11a791cec33aa3535fea2a1e7b42bb7ac23d0aa
Abram Quincy <xxvi.unknown@gmail.com>
2025-07-19 22:35:02 +0800

    Announcement Section Added

---
+++ main.js
@@ -0,0 +1,14 @@
+// Smooth Scroll
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                e.preventDefault();
+                window.scrollTo({
+                    top: target.offsetTop - 100,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
